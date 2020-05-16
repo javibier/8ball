132250ab4c2d716935dd6ca4eba210891d557465
Javier Echevarria <javierechevarria21@yahoo.com>
2020-05-16 14:23:25 -0400

    revise button prompt js

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 // define the method
-    magic8Ball.askQuestion = function(question)
+magic8Ball.askQuestion = function(question)
 
- 
-     // create a random number
-     var randomNumber = Math.random();
- 
-     // make the random number between 0 and the number of items in your array
-     var randomNumberForListOfAnswers = randomNumber * this.listOfAnswers.length;
- 
-     // round down the number to be a whole number
-     var randomIndex = Math.floor(randomNumberForListOfAnswers);
- 
-     // use that number to index a random number from the answers array
-     var answer = this.listOfAnswers[randomIndex];
- 
-     console.log(question);
-     console.log(answer);
+
+// create a random number
+var randomNumber = Math.random();
+
+// make the random number between 0 and the number of items in your array
+var randomNumberForListOfAnswers = randomNumber * this.listOfAnswers.length;
+
+// round down the number to be a whole number
+var randomIndex = Math.floor(randomNumberForListOfAnswers);
+
+// use that number to index a random number from the answers array
+var answer = this.listOfAnswers[randomIndex];
+
+console.log(question);
+console.log(answer);
 
 $(document).ready(function(){
 
@@ -23,5 +23,4 @@ $(document).ready(function(){
     magic8Ball.askQuestion(question);
     };
 
-    $("#questionButton").click( onClick );
-});
+$("#questionButton").click( onClick );
