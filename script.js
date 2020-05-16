cb7cfad7f578525b5ed919418a5d81a935982539
Javier Echevarria <javierechevarria21@yahoo.com>
2020-05-16 15:03:56 -0400

    revise html/js

--- script.js
+++ script.js
@@ -10,13 +10,10 @@ magic8Ball.askQuestion = function(question) {
 
 // create a random number
 var randomNumber = Math.random();
-
 // make the random number between 0 and the number of items in your array
 var randomNumberForListOfAnswers = randomNumber * this.listOfAnswers.length;
-
 // round down the number to be a whole number
 var randomIndex = Math.floor(randomNumberForListOfAnswers);
-
 // use that number to index a random number from the answers array
 var answer = this.listOfAnswers[randomIndex];
 
